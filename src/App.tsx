7a40d84d78a8776ebc9fbc294f43c4bb5fd2bc4f
Bauzi <volodiasyd@gmail.com>
2025-10-22 21:13:06 +0300

    Add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+enum LoadType {
+  All,
+  FirstFive,
+  Red,
+}
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleLoadGoodsBy = (loadType: LoadType) => {
+    let promise: Promise<Good[]>;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    switch (loadType) {
+      case LoadType.All:
+        promise = getAll();
+        break;
+      case LoadType.FirstFive:
+        promise = get5First();
+        break;
+      case LoadType.Red:
+        promise = getRedGoods();
+        break;
+    }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    promise
+      .then(goodsFromServer => setGoods(goodsFromServer))
+      .catch(() => setGoods([]));
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => handleLoadGoodsBy(LoadType.All)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handleLoadGoodsBy(LoadType.FirstFive)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => handleLoadGoodsBy(LoadType.Red)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
